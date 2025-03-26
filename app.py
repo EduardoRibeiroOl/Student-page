8d219a20bf69bab95f687e4289f66431846a4af0
Eduardo Ribeiro <eduardoolive777@gmail.com>
2025-03-26 20:17:47 -0300

    html do login bem feito e o css também feito, ajustes nas rotas e algumas alterações bobas

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
 import mysql.connector 
 #from flask_sqlalchemy import SQLAlchemy
 #from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -13,24 +14,50 @@ app = Flask(__name__)
 conection = mysql.connector.connect(host='localhost', database='localdata', user='root', password='A@a12072007') #Mysql conection
 cursor = conection.cursor() #To using SQL commands
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
-@app.route('/',  methods=['GET', 'POST']) #Initial route
+@app.route("/")
+def root():
+    return redirect("/login")  
+
+
+@app.route("/register")
+def register():
+    
+    if request.method == 'POST':
+        username = request.form["username"]
+        password = request.form["password"]
+
+        cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (username, password))
+        conection.commit()
+        return redirect("/login")  #render the homepage.html template
+    
+    return render_template("register.html") 
+ 
+
+@app.route("/login",  methods=['GET', 'POST']) #Initial route
 def login():
 
+    session.clear() #Clear the session
+    
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
         
-        cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (username, password))
+        #cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (username, password))
+        search = cursor.execute("SELECT * FROM users WHERE name = %s AND password = %s", (username, password))
         conection.commit()
+
         return redirect("/homepage") #redirect to order rote, int his case is the homepage rote
 
     return render_template("login.html")  
 
 
 
-@app.route('/homepage') #Main route 
+@app.route("/homepage") #Main route 
 def homepage():
     
     return render_template("homepage.html")
