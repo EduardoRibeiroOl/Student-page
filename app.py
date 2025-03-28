8b7fdad74bf25a1a179c99bc796b70bade0ef8eb
Eduardo Ribeiro <eduardoolive777@gmail.com>
2025-03-28 15:07:04 -0300

    concertado todas as besteiras que fiz com o git

--- app.py
+++ app.py
@@ -1,40 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
 import mysql.connector 
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
-#from flask_wtf import FlaskForm
-#from wtforms import StringField, PasswordField, SubmitField
-#from wtforms.validators import InputRequired, Length, ValidationError
-#from flask_bcrypt import Bcrypt
-
 
 app = Flask(__name__)
 
-conection = mysql.connector.connect(host='localhost', database='localdata', user='root', password='A@a12072007') #Mysql conection
-cursor = conection.cursor() #To using SQL commands
+
+conection = mysql.connector.connect(
+    host='localhost', database='localdata', user='root', password='A@a12072007'
+)
+cursor = conection.cursor()
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-
 @app.route("/")
 def root():
-    return redirect("/login")  
+    return redirect("/login")
 
-
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    
     if request.method == 'POST':
         username = request.form["username"]
         password = request.form["password"]
 
-        if not username or password:
-            return redirect("/login")
+        if not username or not password:
+            return redirect("/register")
 
-        cursor.execute("SELECT * FROM users WHERE name = %s", (username))
+       
+        cursor.execute("SELECT * FROM users WHERE name = %s", (username,))
         validate_name = cursor.fetchone()
 
         if validate_name:
@@ -42,32 +36,35 @@ def register():
 
         cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (username, password))
         conection.commit()
-        return redirect("/login")  #render the homepage.html template
-    
-    return render_template("register.html") 
- 
+        return redirect("/login")
 
-@app.route("/login",  methods=['GET', 'POST']) 
+    return render_template("register.html")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-
     session.clear() 
-    
+
     if request.method == 'POST':
-        username = request.form["username"]
-        password = request.form["password"]
-        
-        #cursor.execute("INSERT INTO users (name, password) VALUES (%s, %s)", (username, password))
-        search = cursor.execute("SELECT * FROM users WHERE name = %s AND password = %s", (username, password))
-        conection.commit()
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-        return redirect("/homepage") #redirect to order rote, int his case is the homepage rote
+        if not username or not password:
+            return redirect("/login")
 
-    return render_template("login.html")  
+        cursor.execute("SELECT * FROM users WHERE name = %s AND password = %s", (username, password))
+        user = cursor.fetchone()  # Busca um usuário na base de dados
 
+        if user:
+            session["user_id"] = user[0] 
+            return redirect("/homepage")
+        else:
+            return redirect("/login")
 
+    return render_template("login.html")
 
-@app.route("/homepage") 
+@app.route("/homepage")
 def homepage():
-    
-    return render_template("homepage.html")
+    if "user_id" in session:
+        return render_template("homepage.html")
 
+    return redirect("/login")  
